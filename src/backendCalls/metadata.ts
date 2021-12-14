f7a24d3972c59c9a4eef35d426fc9fd48d9928dc
Jordy <31940280+ac-jorellanaf@users.noreply.github.com>
2021-12-14 10:05:02 +0100

    Updated to include facets in back-end call.

--- src/backendCalls/metadata.ts
+++ src/backendCalls/metadata.ts
@@ -13,7 +13,7 @@ const postHeaders = {
 
 export const getAllDatasetsSearchResp: getAllDatasetsSearchRespType = (callbackFunc) => {
     fetch(
-        `${process.env.REACT_APP_SVC_METADATA_SEARCH_URL}/rpc/search?document_type=Dataset&return_facets=false&skip=0&limit=0`,
+        `${process.env.REACT_APP_SVC_METADATA_SEARCH_URL}/rpc/search?document_type=Dataset&return_facets=true&skip=0&limit=0`,
         {
             method: 'POST',
             headers: postHeaders,
